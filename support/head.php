5b05d67bc788b6e7813af72e3be623770c892501
pspfreak <alfd902@gmail.com>
2022-12-18 12:32:49 -0500

    Add files via upload

--- support/head.php
+++ support/head.php
@@ -5,6 +5,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.js"></script>
 <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
--- support/nav.php
+++ support/nav.php
@@ -1,8 +1,8 @@
-  <nav class="black" role="navigation">
+  <nav class="" role="navigation">
     <div class="nav-wrapper container">
       <a id="logo-container" href="#" class="brand-logo"><img src="https://pspfreak.xyz/assets/img/psp.png" width="62px"></a>
       <ul class="right">
-        <li><a href="#">Join Now</a></li>
-		<li><a href="#">RIP</a></li>
+		<li><a href="index.php">Home</a></li>
+        <li><a href="gen.php">Generate</a></li>
       </ul>
   </nav>
